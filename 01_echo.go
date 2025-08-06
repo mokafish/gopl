7f3564cae28661fb36cc7dcce9319bd427f90612
mokafish <mokafish@users.noreply.github.com>
2025-08-06 14:13:46 +0800

    test: test

--- 01_echo.go
+++ 01_echo.go
@@ -5,9 +5,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
+	fmt.Println(strings.Join(os.Args, " "))
+}
+
+func main1() {
 	var s, sep string
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module demo
 
 go 1.24.5
